947327e0fdc07889167220d4c5dffa94345e0a89
karlkori <skaller@tut.by>
2018-01-15 01:47:02 +0300

    added new design, changed js

--- mpa/mpa-script.js
+++ mpa/mpa-script.js
@@ -1,5 +1,7 @@
 var search = window.location.search.substr(1),
-	keys = {};
+	keys = {},
+  sum = lastButton = null,
+  lastRightView = ".donate-right";
       
 search.split('&').forEach(function(item) {
 	item = item.split('=');
@@ -7,7 +9,9 @@ search.split('&').forEach(function(item) {
 });
  
 var dataConfig;
+
 loadDataConfig();
+
 function loadConfig(){
 
      getOutputFromRequest('/mpa/ajax-config.php', setConfigHTML)
@@ -16,6 +20,7 @@ function loadConfig(){
 
 }
 function loadDataConfig(){
+
    getOutputFromRequest('/mpa/ajax-config.php', setConfigData);
 
 }
@@ -28,21 +33,21 @@ function setConfigData(data){
 
 function setConfigHTML(data){
 
-      document.getElementById("mpa-title").innerHTML = data.titleDonate;     
-      document.getElementById("mpa-text").innerHTML = data.shortTextDonate;
-      document.getElementById("mpa-sum").innerHTML = data.sumDonate;
-      document.getElementById("mpa-currency").innerHTML = data.currencyDonate;
-      
+  document.getElementsByClassName("donate-title")[0].innerText = data.titleDonate;     
+  document.querySelector(".donate-desc div").innerText = data.shortTextDonate;
+  document.getElementsByClassName("donate-sum")[0].innerText = data.sumDonate;
       
 }
           
-function getBePaidJS(data){
+function getBePaidJS(data) {
+  changeRightView(".donate-form");
+
                        var options = {
                        type: 'inline',
-                       id: 'donate-scope',
+                       id: 'donate-form',
                        url: data.checkout.redirect_url,
                        style: dataConfig.css,
-                       size: { width: 480, height: 365 }
+                       size: { width: 390, height: 280 }
                        };
                        var pf = new BeGateway(options);
                        pf.buildForm();
@@ -65,19 +70,6 @@ function getOutputFromRequest(url, callback) {
       request.send();
  }
 
-function radiobutton(){
-             document.getElementById("5").checked = false;
-             document.getElementById("10").checked = false;
-             document.getElementById("15").checked = false;
-             document.getElementById("20").checked = false;
-             document.getElementById("50").checked = false;
-             document.getElementById("100").checked = false;
-                 
-}
-function removeinput(){
-        document.getElementById("free-donate").value = "";
-
-}
 function backbutton(){
         document.getElementById("donate-scope").innerHTML = '<ul class="button-storage"><li class="custom-donate"><input type="radio" name="custom-button-donate" onclick="removeinput()" id="5" class="custom-input" value="5"><label for="5" class="custom-button">5 руб.</label></li><li class="custom-donate"><input type="radio" name="custom-button-donate" onclick="removeinput()" id="10" class="custom-input" value="10"><label for="10" class="custom-button">10 руб.</label></li><li class="custom-donate"><input type="radio" name="custom-button-donate" onclick="removeinput()" id="15" class="custom-input" value="15"><label for="15" class="custom-button">15 руб.</label></li><li class="custom-donate"><input type="radio" name="custom-button-donate" onclick="removeinput()" id="20" class="custom-input" value="20"><label for="20" class="custom-button">20 руб.</label></li><li class="custom-donate"><input type="radio" name="custom-button-donate" onclick="removeinput()" id="50" class="custom-input" value="50"><label for="50" class="custom-button">50 руб.</label></li><li class="custom-donate"><input type="radio" name="custom-button-donate" onclick="removeinput()" id="100" class="custom-input" value="100"><label for="100" class="custom-button">100 руб.</label></li><li id="custom-free-donate"><input type="text" name="free-donate" id="free-donate" onclick="radiobutton()" placeholder="Iншая сума" title="Калі ласка, увядзіце неабходную суму"></li><li id="li-button-donate"><button id="button-donate" onclick="submitbutton()">Ахвяруй!</button></li></ul>';
         document.getElementById("mpa-rules").innerHTML = '<a href="javascript:PopUpShow()">Апiсанне спосабу аплаты</a>';
@@ -85,94 +77,79 @@ function backbutton(){
 function back(){
         document.location.reload(false);
 }
-function submitbutton(){
 
-             
-
-             var iscustom = false;
-             if (document.getElementById("5").checked) {iscustom = document.getElementById("5").value;}
-             var customdonate1 = document.getElementById("5").checked;
-             if (document.getElementById("10").checked) {iscustom = document.getElementById("10").value;}
-             var customdonate2 = document.getElementById("10").checked;
-             if (document.getElementById("15").checked) {iscustom = document.getElementById("15").value;}
-             var customdonate3 = document.getElementById("15").checked;
-             if (document.getElementById("20").checked) {iscustom = document.getElementById("20").value;}
-             var customdonate4 = document.getElementById("20").checked;
-             if (document.getElementById("50").checked) {iscustom = document.getElementById("50").value;}
-             var customdonate5 = document.getElementById("50").checked;
-             if (document.getElementById("100").checked) {iscustom = document.getElementById("100").value;}
-             var customdonate6 = document.getElementById("100").checked;
-              
-             var freedonate = document.getElementById("free-donate");
-             var isok = customdonate1 + customdonate2 + customdonate3 + customdonate4 + customdonate5 + customdonate6;
-             var regexp = /^[1-9]\d*$/;
-             
-            
-            if(!isok && !freedonate.value){
-                 
-                 document.getElementById("mpa-rules").innerHTML = "Сума не абрана альбо не уведзена!";
-                
-             }                      
-             else if(!regexp.test(freedonate.value) && freedonate.value ){
-               document.getElementById("mpa-rules").innerHTML = 'У поле "Iншая сума" трэба уводзiць цэлыя лiкi!';  
-               
-               
-               
-             }
-             else if(freedonate.value >  dataConfig.maxDonate){
-             
-                       document.getElementById("mpa-rules").innerHTML = 'Ахвяраванне не можа быць большым за '+ dataConfig.maxDonate +' руб!';
-             
-             
-             }
-             else if(freedonate.value && freedonate.value < dataConfig.minDonate ){
-             
-                       document.getElementById("mpa-rules").innerHTML = 'Ахвяраванне не можа быць меншым за '+ dataConfig.minDonate +' руб!';
-             
-             
-             }
-                                                                                                         
-              else {
-                  document.getElementById("main-conteiner").setAttribute('style','height: 365px');
-                  document.getElementById("button-back").innerHTML = '<img onclick="back()" src="mpa/img/button-back.png"><p style="font-size:60%">Назад</p>';
-                  document.getElementById("donate-scope").innerHTML ='';
-                  document.getElementById("mpa-rules").innerHTML = '';
-                  if(iscustom){                                                                                         
-                     var url = 'mpa/donate.php?donate=' + iscustom;
-                  
-                  }else{ var url = 'mpa/donate.php?donate=' + freedonate.value; }  
-                  getOutputFromRequest(url,getBePaidJS);
-                  
-             
-             }
+function takeSum(e) {
+  if ( e.target.tagName === "BUTTON" && e.target.hasAttribute("data-sum") ) {
+    sum = +e.target.getAttribute("data-sum");
+    if ( lastButton ) lastButton.style.backgroundColor = "#f9f9f9";
+    e.target.style.backgroundColor = "#ccff00";
+    lastButton = e.target;
+  } else if ( e.target.tagName === "INPUT" ) {
+    if ( lastButton ) lastButton.style.backgroundColor = "#f9f9f9";
+    if ( e.target.value > 0 ) sum = +e.target.value;
+  }
 }
+
+function submitbutton() {
+
+  if( !sum ) {
+
+    document.getElementById("mpa-rules").innerHTML = "Сума не абрана альбо не ўведзена!";
+                
+  } else if ( sum > dataConfig.maxDonate) {
+             
+    document.getElementById("mpa-rules").innerHTML = 'Ахвяраванне не можа быць большым за '+ dataConfig.maxDonate +' руб!';
+             
+  } else if ( sum < dataConfig.minDonate ) {
+             
+    document.getElementById("mpa-rules").innerHTML = 'Ахвяраванне не можа быць меньшым за '+ dataConfig.minDonate +' руб!';
+             
+  } else {
+
+    var url = 'mpa/donate.php?donate=' + sum;
+    getOutputFromRequest(url, getBePaidJS);
+
+}
+
+}
+
+function changeRightView(newView) {
+  document.querySelector(lastRightView).style.display = "none";
+  document.querySelector(newView).style.display = "flex";
+  lastRightView = newView;
+}
+
 window.onload = function() {
         if(keys.message){
-            document.getElementById("mpa-rules").innerHTML = '<button onclick="backbutton()">Паспрабаваць яшчэ раз</button>';            
             switch (keys.message) {
               case '1':
-              document.getElementById("donate-scope").innerHTML ="<h3 style='margin-top: 12%'>Ахвяраванне паспяхова здейснена</h3><h1 style='text-align: center'>Дзякуй за падтрымку!</h1>";
-              document.getElementById("mpa-rules").innerHTML = '';
+              changeRightView(".donate-success");
               break;
               case '2':
-              document.getElementById("donate-scope").innerHTML = "<h3 style='margin-top: 15%' >Прабачце, транзакция была адхiлена банкам</h3>";
+              changeRightView(".donate-error-bank");
               break;
               case '3':
-              document.getElementById("donate-scope").innerHTML = "<h3 style='margin-top: 14%' >Прабачце, транзакцыя была адхiлена</h3><h3>з прычын тэхнiчнага збою</h3>" ;
+              changeRightView(".donate-error-tech");
               break;
            }   
         }
+  document.querySelector(".donate-right").addEventListener("click", takeSum);
+  document.querySelector(".donate-call input").addEventListener("focusout", takeSum);
 };
-  $(document).ready(function(){
-        //Скрыть PopUp при загрузке страницы    
-        PopUpHide();
-    });
-    //Функция отображения PopUp
-    function PopUpShow(){
-        $("#popup1").show();
-    }
-    //Функция скрытия PopUp
-    function PopUpHide(){
-        $("#popup1").hide();
-    }
- loadConfig();     
+
+$(document).ready(function() {
+  //Скрыть PopUp при загрузке страницы    
+  PopUpHide();
+  });
+
+//Функция отображения PopUp
+function PopUpShow() {
+  $("#popup1").show();
+}
+
+//Функция скрытия PopUp
+function PopUpHide() {
+  $("#popup1").hide();
+}
+
+loadConfig();
